a75704df5be501c010424b8eddb699c53a7885cb
Patcharapoln <patcharapol.n@ku.th>
2020-05-17 13:41:30 +0700

    Init project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Hello World'))
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log("Server started")
+})
+
+module.exports = app
