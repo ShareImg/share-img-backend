bdbd10ee7f7491a9ed293c6552f6010207868735
Pimwalun <janny25522009@hotmail.com>
2020-05-23 23:40:06 +0700

    try s3

--- src/s3.js
+++ src/s3.js
@@ -1,18 +1,30 @@
 
 import AWS from 'aws-sdk'
 
-const BUCKET = 'shareimg-ske14'
+const BUCKET = 'shareimg-bucket'
 const REGION = 'us-east-1'
-const ACCESS_KEY = 'ASIAVIJVTJHCLPTTDT3A'
-const SECRET_KEY = 'esIVPLks00iFjIcoKPCARMvW8Lv2LVcRNEuBRBVc'
+const ACCESS_KEY = 'ASIA2X75E2CZECFVIL43'
+const SECRET_KEY = 'JfQpPygc8sWCAkGqKcNjhqzTaZAfuWLO+tYI9gwg'
 
 const localImage = './cat.png'
 const imageRemoteName = `catImage_${new Date().getTime()}.png`
 
+// const s3Client = new AWS.S3({
 const s3 = new AWS.S3({
   accessKeyId: ACCESS_KEY,
   secretAccessKey: SECRET_KEY,
+  Bucket: BUCKET,
   region: REGION
 })
+
+// const uploadParams = {
+//   Bucket: BUCKET, 
+//   Key: '', // pass key
+//   Body: null, // pass file body
+// };
+
+// const s3 = {};
+// s3.s3Client = s3Client;
+// s3.uploadParams = uploadParams;
 
 export default s3
