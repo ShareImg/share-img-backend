eb5898e8bbe5d3bfa1c144b447620caf8d6c697f
Patcharapoln <patcharapol.n@ku.th>
2020-05-30 22:32:31 +0700

    add db

--- src/connection.js
+++ src/connection.js
@@ -7,6 +7,7 @@ var mysqlconnect = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: 'shareimg'
 });
 
 mysqlconnect.connect(function (err) { 
