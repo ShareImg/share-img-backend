6f8a26c67a6f5a1ca8e02f6fd541b00e091a6660
Pimwalun <janny25522009@hotmail.com>
2020-05-31 19:09:05 +0700

    clean function in photo.js

--- src/routes/Photo.js
+++ src/routes/Photo.js
@@ -23,36 +23,7 @@ const validateUrl = (str) => {
   return pattern.test(str);
 }
 
-const ImgUpload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'shareimg-bucket',
-    acl: 'public-read',
-    key: function (req, file, cb) {
-      cb(null, path.basename( file.originalname, path.extname( file.originalname ) ) + '-' + Date.now() + path.extname( file.originalname ) )
-    }
-  }),
-  limits:{ fileSize: 2000000 }, // In bytes: 2000000 bytes = 2 MB
-  fileFilter: function( req, file, cb ){
-    checkFileType( file, cb );
-  }
- }).single('file');
-
 const router = express.Router();
-
-function checkFileType( file, cb ){
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test( path.extname( file.originalname ).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test( file.mimetype );
-  if( mimetype && extname ){
-    return cb( null, true );
-  } else {
-    cb( 'Error: Images Only!' );
-  }
- }
 
 router.get('/', async(req, res) => {
   mysqlconnect.query("SELECT * FROM shareimg.Users \
